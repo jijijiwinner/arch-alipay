79a868fe0a1dc237d37083939d9d3f42637ae8cb
jijianmei <jianmei.ji@zhonganbio.com>
2019-02-20 18:37:53 +0800

    推送代码

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -1,6 +1,6 @@
 // pages/movies/more-movie/more-movie.js
-var app = getApp()
-var util = require('../../../utils/util.js')
+var app = getApp();
+var util = require("../../../utils/util.js");
 Page({
   data: {
     movies: [],
@@ -8,55 +8,52 @@ Page({
     requestUrl: "",
     totalCount: 0,
     isEmpty: true,
-    hiddenLoading:false,
-    disabledRemind:false
+    hiddenLoading: false,
+    disabledRemind: false
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     var category = options.category;
     this.data.navigateTitle = category;
     var dataUrl = "";
     switch (category) {
       case "正在热映":
-        dataUrl = app.globalData.doubanBase +
-          "/v2/movie/in_theaters";
+        dataUrl = app.globalData.doubanBase + "/v2/movie/in_theaters";
         break;
       case "即将上映":
-        dataUrl = app.globalData.doubanBase +
-          "/v2/movie/coming_soon";
+        dataUrl = app.globalData.doubanBase + "/v2/movie/coming_soon";
         break;
       case "豆瓣Top250":
         dataUrl = app.globalData.doubanBase + "/v2/movie/top250";
         break;
     }
-     my.setNavigationBar({
-          title: category,
-          color:"#fff",
-          backgroundColor: '#314C6D'
-    })
+    my.setNavigationBar({
+      title: category,
+      color: "#fff",
+      backgroundColor: "#314C6D"
+    });
     this.data.requestUrl = dataUrl;
-    console.log(this.data.requestUrl)
+    console.log(this.data.requestUrl);
     // util.http(dataUrl, this.processDoubanData)
-    var that = this
-    var url = dataUrl
+    var that = this;
+    var url = dataUrl;
     my.httpRequest({
       url: url,
-      method: 'GET',
+      method: "GET",
       header: {
         "Content-Type": "json"
       },
-      success: function (res) {
-        that.processDoubanData(res.data)
+      success: function(res) {
+        that.processDoubanData(res.data);
         // console.log(res.data)
       },
-      fail: function (error) {
+      fail: function(error) {
         // fail
-        console.log(error)
+        console.log(error);
       }
-    })
+    });
   },
-  onPullDownRefresh: function (event) {
-    var refreshUrl = this.data.requestUrl +
-      "?star=0&count=20"
+  onPullDownRefresh: function(event) {
+    var refreshUrl = this.data.requestUrl + "?star=0&count=20";
     this.data.movies = {};
     this.data.isEmpty = true;
     this.data.totalCount = 0;
@@ -64,39 +61,39 @@ Page({
     util.http(refreshUrl, this.processDoubanData);
     my.showNavigationBarLoading();
   },
-  onReachBottom: function (event) {
+  onReachBottom: function(event) {
     // 上滑加载
-    var nextUrl = this.data.requestUrl +
-      "?start=" + this.data.totalCount + "&count=20";
-    console.log(123)
-    var that=this
+    var nextUrl =
+      this.data.requestUrl + "?start=" + this.data.totalCount + "&count=20";
+    console.log(123);
+    var that = this;
     my.httpRequest({
       url: nextUrl,
-      method: 'GET',
+      method: "GET",
       header: {
         "Content-Type": "json"
       },
-      success: function (res) {
-        that.processDoubanData(res.data)
+      success: function(res) {
+        that.processDoubanData(res.data);
         // console.log(res.data)
       },
-      fail: function (error) {
+      fail: function(error) {
         // fail
-        console.log(error)
+        console.log(error);
       }
-    })
-    my.showNavigationBarLoading()
+    });
+    my.showNavigationBarLoading();
   },
-  processDoubanData: function (moviesDouban) {
+  processDoubanData: function(moviesDouban) {
     var movies = [];
     //没有更多啦
-    if(moviesDouban.subjects.length<=0){
+    if (moviesDouban.subjects.length <= 0) {
       var _this = this;
-      if(!_this.data.disabledRemind){
+      if (!_this.data.disabledRemind) {
         _this.setData({
           disabledRemind: true
         });
-        setTimeout(function(){
+        setTimeout(function() {
           _this.setData({
             disabledRemind: false
           });
@@ -116,16 +113,15 @@ Page({
         average: subject.rating.average,
         coverageUrl: subject.images.large,
         movieId: subject.id
-      }
-      movies.push(temp)
+      };
+      movies.push(temp);
     }
-    var totalMovies = {}
+    var totalMovies = {};
 
     //如果要绑定新加载的数据，那么需要同旧有的数据合并在一起
     if (!this.data.isEmpty) {
       totalMovies = this.data.movies.concat(movies);
-    }
-    else {
+    } else {
       totalMovies = movies;
       this.data.isEmpty = false;
     }
@@ -134,22 +130,22 @@ Page({
     });
     this.data.totalCount += 20;
     my.hideNavigationBarLoading();
-    my.stopPullDownRefresh()
+    my.stopPullDownRefresh();
     this.setData({
-      hiddenLoading:true
-    })
+      hiddenLoading: true
+    });
   },
 
-  onReady: function (event) {
+  onReady: function(event) {
     my.setNavigationBar({
       title: this.data.navigateTitle
-    })
+    });
   },
 
-  onMovieTap: function (event) {
+  onMovieTap: function(event) {
     var movieId = event.currentTarget.dataset.movieid;
     my.navigateTo({
-      url: '../movie-detail/movie-detail?id=' + movieId
-    })
-  },
-})
+      url: "../movie-detail/movie-detail?id=" + movieId
+    });
+  }
+});
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,4 +1,5 @@
-var util = require('../../utils/util.js')
+var util = require("../../utils/util.js");
+const arch = require("../../arch/arch.js");
 var app = getApp();
 Page({
   // RESTFul API JSON
@@ -10,90 +11,90 @@ Page({
     top250: {},
     searchResult: {},
     containerShow: true,
-    searchPanelShow: false,
+    searchPanelShow: false
   },
-    onShareAppMessage: function () {
+  onShareAppMessage: function() {
     return {
-      title: '光与影',
-      desc: '进入搜索电影吧',
-      path: '/pages/movies/movies'
-    }
+      title: "光与影",
+      desc: "进入搜索电影吧",
+      path: "/pages/movies/movies"
+    };
   },
-  onLoad: function (event) {
-    var inTheatersUrl = app.globalData.doubanBase +
-      "/v2/movie/in_theaters" + "?start=0&count=6";
-    var comingSoonUrl = app.globalData.doubanBase +
-      "/v2/movie/coming_soon" + "?start=0&count=6";
-    var top250Url = app.globalData.doubanBase +
-      "/v2/movie/top250" + "?start=0&count=6";
+  onLoad: function(event) {
+    var inTheatersUrl =
+      app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=6";
+    var comingSoonUrl =
+      app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=6";
+    var top250Url =
+      app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=6";
 
     this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
     this.getMovieListData(comingSoonUrl, "comingSoon", "即将上映");
     this.getMovieListData(top250Url, "top250", "豆瓣Top250");
-    my.setNavigationBar({
-          title: '光与影',
-          color:"#fff",
-          backgroundColor: '#314C6D'
-    });
+    // my.setNavigationBar({
+    //   title: "光与影",
+    //   color: "#fff",
+    //   backgroundColor: "#314C6D"
+    // });
+    arch.ui.setNavigationBarTitle("光与影hhh");
   },
-  
-  onMoreTap: function (event) {
+
+  onMoreTap: function(event) {
     var category = event.currentTarget.dataset.category;
     my.navigateTo({
       url: "more-movie/more-movie?category=" + category
-    })
+    });
   },
 
-  onMovieTap:function(event){
+  onMovieTap: function(event) {
     var movieId = event.currentTarget.dataset.movieId;
     // console.log(event.currentTarget)
     my.navigateTo({
-      url: "movie-detail/movie-detail?id="+movieId
-    })
+      url: "movie-detail/movie-detail?id=" + movieId
+    });
   },
 
-  getMovieListData: function (url, settedKey, categoryTitle) {
-    my.showNavigationBarLoading()
+  getMovieListData: function(url, settedKey, categoryTitle) {
+    my.showNavigationBarLoading();
     var that = this;
     my.httpRequest({
       url: url,
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: {
         "Content-Type": "json"
       },
-      success: function (res) {
-        that.processDoubanData(res.data, settedKey, categoryTitle)
+      success: function(res) {
+        that.processDoubanData(res.data, settedKey, categoryTitle);
       },
-      fail: function (error) {
+      fail: function(error) {
         // fail
-        console.log(error)
+        console.log(error);
       }
-    })
+    });
   },
 
-  onCancelImgTap: function (event) {
-      this.setData({
-        containerShow: true,
-        searchPanelShow: false,
-        searchResult:{}
-      }
-    )
+  onCancelImgTap: function(event) {
+    this.setData({
+      containerShow: true,
+      searchPanelShow: false,
+      searchResult: {}
+    });
   },
 
-  onBindFocus: function (event) {
+  onBindFocus: function(event) {
     this.setData({
       containerShow: false,
       searchPanelShow: true
-    })
+    });
   },
 
-  onBindBlur: function (event) {
+  onBindBlur: function(event) {
     var text = event.detail.value;
     var searchUrl = app.globalData.doubanBase + "/v2/movie/search?q=" + text;
     this.getMovieListData(searchUrl, "searchResult", "");
   },
 
-  processDoubanData: function (moviesDouban, settedKey, categoryTitle) {
+  processDoubanData: function(moviesDouban, settedKey, categoryTitle) {
     var movies = [];
     for (var idx in moviesDouban.subjects) {
       var subject = moviesDouban.subjects[idx];
@@ -108,16 +109,16 @@ Page({
         average: subject.rating.average,
         coverageUrl: subject.images.large,
         movieId: subject.id
-      }
-      movies.push(temp)
+      };
+      movies.push(temp);
     }
     var readyData = {};
     readyData[settedKey] = {
       categoryTitle: categoryTitle,
       movies: movies
-    }
+    };
     this.setData(readyData);
-    console.log(readyData)
+    console.log(readyData);
     my.hideNavigationBarLoading();
   }
-})
+});
